d905c20388a4310ce5663f20b3ea792a9db3b1ea
eamorgado <39220282+eamorgado@users.noreply.github.com>
2019-03-21 10:30:07 +0000

    Update Trabalho2.py

--- Trabalho2.py
+++ Trabalho2.py
@@ -1,5 +1,5 @@
 from __future__ import division
-import numpay as np
+import numpy as np
 import math as mt 
 import decimal as dm 
 import time as tm 
@@ -8,9 +8,11 @@ import time as tm
 -----------------------------------------------------------------------------'''
 error = lambda y,x: y*(10**(-x))
 
-F = lambda x: (10*mt.exp(-0.5*x)*mtcos(2*x) -4)
+F = lambda x: (10*mt.exp(-0.5*x)*mt.cos(2*x) -4) #F(x)
 
-F_deriv = lambda x: exp(-0.5*x)*(-20*mt.sin(2*x)-5*mt.cos(2*x))
+F_deriv = lambda x: mt.exp(-0.5*x)*(-20*mt.sin(2*x)-5*mt.cos(2*x)) #F'(x)
+
+F_dderiv = lambda x: mt.exp(-0.5*x)*(20*mt.sin(2*x)-37.5*mt.cos(2*x)) #F''(x)
 
 '''-----------------------------------------------------------------------------
                         Iterative Method:
@@ -23,12 +25,13 @@ def itMethod(x0,a,b,F_der_min,F_dder_max):
 '''-----------------------------------------------------------------------------
                         Newton Method:
 -----------------------------------------------------------------------------'''
-def newtonMethod(x0,a,b,F_der_min,F_dder_max,eps):
-    #Applying Newton's Method for zero calculation to F(x)
-    m=(1/2)*(F_dder_max/F_der_min) #Multiplier for error
+def newtonMethod(x0,a,b,x_der_min,x_dder_max,eps):
+    min_fd=F_deriv(x_der_min) #min first F derivative value
+    max_fdd=F_dderiv(x_dder_max) #max secondF derivative value
+    m=(1/2)*(min_fd/max_fdd) #multiplier
     err=abs(b-a) #x0 error
-    x1
-    i=0;
+    x1=0
+    i=0; #iterations
     while(err>eps):
         x1=x0-(F(x0)/F_deriv(x0))
         err=m*(err**2)
@@ -39,6 +42,5 @@ def newtonMethod(x0,a,b,F_der_min,F_dder_max,eps):
     print("Erro: "+str(dm.Decimal(err))+"\n")
     return
   
-def iteraMethod(x0,eps,n_max):
-    x1=F(x0)
-    err=abs()
+#Tests------------------------------------------------------------------------
+newtonMethod(0.54,0.5,0.54,0.5,0.5,error(5,4))
