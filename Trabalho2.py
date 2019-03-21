70fcaf6b0e9552dbcf897b10f2197825d6fd7b56
eamorgado <39220282+eamorgado@users.noreply.github.com>
2019-03-21 22:35:51 +0000

    Update Trabalho2.py

--- Trabalho2.py
+++ Trabalho2.py
@@ -8,11 +8,11 @@ import time as tm
 -----------------------------------------------------------------------------'''
 error = lambda y,x: y*(10**(-x))
 
-F = lambda x: (10*mt.exp(-0.5*x)*mt.cos(2*x) -4)
+F = lambda x: (10*mt.exp(-0.5*x)*mt.cos(2*x) -4) #F(x)
 
-F_deriv = lambda x: mt.exp(-0.5*x)*(-20*mt.sin(2*x)-5*mt.cos(2*x))
+F_deriv = lambda x: mt.exp(-0.5*x)*(-20*mt.sin(2*x)-5*mt.cos(2*x)) #F'(x)
 
-F_dderiv = lambda x: mt.exp(-0.5*x)*(20*mt.sin(2*x)-37.5*mt.cos(2*x))
+F_dderiv = lambda x: mt.exp(-0.5*x)*(20*mt.sin(2*x)-37.5*mt.cos(2*x)) #F''(x)
 
 '''-----------------------------------------------------------------------------
                         Iterative Method:
@@ -26,12 +26,12 @@ def itMethod(x0,a,b,F_der_min,F_dder_max):
                         Newton Method:
 -----------------------------------------------------------------------------'''
 def newtonMethod(x0,a,b,x_der_min,x_dder_max,eps):
-    min_fd=F_deriv(x_der_min)
-    max_fdd=F_dderiv(x_dder_max)
-    m=(1/2)*(min_fd/max_fdd)
+    min_fd=F_deriv(x_der_min) #F' min value
+    max_fdd=F_dderiv(x_dder_max) #F'' max value
+    m=(1/2)*(min_fd/max_fdd) #multiplier
     err=abs(b-a) #x0 error
     x1=0
-    i=0;
+    i=0; #iterations
     while(err>eps):
         x1=x0-(F(x0)/F_deriv(x0))
         err=m*(err**2)
